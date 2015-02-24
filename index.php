6fdaccdf92f3b6e0a1a75f79b077f1e579cc1004
trevor <trevor@trevweb.me.uk>
2015-02-24 23:24:42 +0000

    Removed unnecessary Google Analytics code.

--- index.php
+++ index.php
@@ -45,6 +45,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
-<?php include_once 'ga.php'; ?>
 </body>
 </html>
